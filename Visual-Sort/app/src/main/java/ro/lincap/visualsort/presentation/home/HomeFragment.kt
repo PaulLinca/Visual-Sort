73f2c808427e7fbe3a3836aab1582820c059a730
PaulLinca <paultudorlinca@gmail.com>
2020-10-16 20:32:10 +0300

    Some ui changes

--- Visual-Sort/app/src/main/java/ro/lincap/visualsort/presentation/home/HomeFragment.kt
+++ Visual-Sort/app/src/main/java/ro/lincap/visualsort/presentation/home/HomeFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
