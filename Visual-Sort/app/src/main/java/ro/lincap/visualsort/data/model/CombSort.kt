ccced31ff77426de316a59cba60413ce3e7b57b1
PaulLinca <paultudorlinca@gmail.com>
2020-11-12 15:23:46 +0200

    Added comb sort color

--- Visual-Sort/app/src/main/java/ro/lincap/visualsort/data/model/CombSort.kt
+++ Visual-Sort/app/src/main/java/ro/lincap/visualsort/data/model/CombSort.kt
@@ -31,19 +31,30 @@ class CombSort : ISortingAlgorithm
 
             for(index in 0 until listCopy.size - gap)
             {
+                highlightedValues.postValue(arrayListOf(Pair(gap.toFloat(), Constants.ORANGE), Pair(index.toFloat(), Constants.YELLOW), Pair((index + gap).toFloat(), Constants.YELLOW)))
+                delay(500 - speed.value!!.toLong() + 1)
+
                 if(listCopy[index].y > listCopy[index + gap].y)
                 {
+                    highlightedValues.postValue(arrayListOf(Pair(gap.toFloat(), Constants.ORANGE), Pair(index.toFloat(), Constants.RED), Pair((index + gap).toFloat(), Constants.RED)))
+
                     val temp = listCopy[index].y
                     listCopy[index].y = listCopy[index + gap].y
                     listCopy[index + gap].y = temp
 
                     swapped = true
                 }
+                else
+                {
+                    highlightedValues.postValue(arrayListOf(Pair(gap.toFloat(), Constants.ORANGE), Pair(index.toFloat(), Constants.GREEN), Pair((index + gap).toFloat(), Constants.GREEN)))
+                }
 
                 listToSort.postValue(listCopy)
                 delay(500 - speed.value!!.toLong() + 1)
             }
         }
+
+        highlightedValues.postValue((0..listCopy.lastIndex).toMutableList().map { int -> Pair(int.toFloat(), Constants.PURPLE) })
     }
 
     private fun getNextGap(gap: Int): Int
