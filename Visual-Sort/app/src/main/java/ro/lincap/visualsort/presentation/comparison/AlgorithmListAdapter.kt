0fb202e9db4997037d3941f230ccbbe893646693
PaulLinca <paultudorlinca@gmail.com>
2020-11-17 16:58:03 +0200

    Setup charts in the lsit

--- Visual-Sort/app/src/main/java/ro/lincap/visualsort/presentation/comparison/AlgorithmListAdapter.kt
+++ Visual-Sort/app/src/main/java/ro/lincap/visualsort/presentation/comparison/AlgorithmListAdapter.kt
@@ -1,12 +1,21 @@
 package ro.lincap.visualsort.presentation.comparison
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.github.mikephil.charting.charts.BarChart
+import com.github.mikephil.charting.data.BarData
+import com.github.mikephil.charting.data.BarDataSet
+import com.github.mikephil.charting.data.BarEntry
+import kotlinx.android.synthetic.main.algorithm_card.view.*
 import ro.lincap.visualsort.R
 import ro.lincap.visualsort.data.model.ISortingAlgorithm
 import ro.lincap.visualsort.databinding.AlgorithmCardBinding
+import ro.lincap.visualsort.util.configureChartAppearance
+import java.lang.IndexOutOfBoundsException
+import kotlin.random.Random
 
 class AlgorithmListAdapter(private val sortingAlgorithms: List<ISortingAlgorithm>): RecyclerView.Adapter<AlgorithmListAdapter.AlgorithmViewHolder>()
 {
@@ -28,5 +37,32 @@ class AlgorithmListAdapter(private val sortingAlgorithms: List<ISortingAlgorithm
     {
         val currentAlgorithm = sortingAlgorithms[position]
         holder.binding.algorithm = currentAlgorithm
+
+        val chart = holder.itemView.card_barChart
+        chart.configureChartAppearance()
+        initChart(chart)
+    }
+
+    private fun initChart(barChart: BarChart)
+    {
+        val entries = ArrayList<BarEntry>()
+        for(chartPosition in 0..51)
+        {
+            val randomValue = Random.nextInt(1, 51).toFloat()
+            entries.add(BarEntry(chartPosition.toFloat(), randomValue))
+        }
+
+        // Create the data set
+        val dataSet = BarDataSet(entries, "Values to be ordered")
+
+        // Disable the bar value labels
+        dataSet.setDrawValues(false)
+
+        // Set the new chart data
+        barChart.data = BarData(dataSet)
+
+        // Redraw the chart
+        barChart.notifyDataSetChanged()
+        barChart.invalidate()
     }
 }
